d60d76a69c7767e4e051365a12778655de53cc2c
Your Name <you@example.com>
2018-08-20 21:15:16 +0300

    fixes2

--- view.php
+++ view.php
@@ -10,6 +10,15 @@
 </head>
 
 <body>
+
+
+  <div class="container">
+    <div class="row bg-info mt-4">
+      <h1>
+GRID DIRECTIONS WITHOUT JSCRIPT OR MASONRY</h1>
+    </div>
+  </div>
+  <hr />
   <div class="container">
     <div class="row">
       <h3>Normal Echo Loop from left to right with col-4 bootstrap divs</h3>
@@ -30,7 +39,7 @@
 
   <div class="container">
     <div class="row">
-      <h3>Same variables With right to left direction with col-4 bootstrap divs<br/>First Column echo evens Numbers <br />Second Column odd numbers</h3>
+      <h3>Same variables With right to left direction with col-4 bootstrap divs<br/>First Column => evens Element id <br />Second Column odd Element Id</h3>
 
 
       <?php  // if ($x % 2 == 0) { //code }
@@ -73,9 +82,46 @@
 
   </div>
 
+<hr />
+
+  <div class="container">
+    <div class="row">
+            <h3>Same variables With top to bottom from left to right direction with col-4 bootstrap divs<br/><br/></h3>
+
+<?php $totalelements=13; $elements_first=round($totalelements/2);$elements_second=$totalelements-$elements_first;
+echo $elements_first."<br />";
+echo $elements_second."<br />";
+//2 columns?>
 
 
+      <div class="row  col-6">
+        <?php  for ($x=0; $x<=$elements_first;$x++) {
+    ?>
 
+        <div class="col-12 bg-warning my-2 ">
+          Content Number <strong><?=$x?></strong><br />
+        </div>
+
+        <?php
+}?>
+
+      </div>
+
+      <div class="row  col-6">
+
+        <?php  for ($x=$elements_first+1; $x<=$totalelements;$x++) {
+        ?>
+        <div class="col-12 bg-primary my-2 ">
+          Content Number <strong><?=$x?></strong><br />
+        </div>
+
+        <?php
+    }?>
+
+      </div>
+
+    </div>
+    </div>
 
 
 
